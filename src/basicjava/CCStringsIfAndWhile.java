dea2d2af2684b9380e2186973d68286e464742cd
shahbazdhaliwal <107386192+shahbazdhaliwal@users.noreply.github.com>
2023-01-29 13:59:26 -0700

    Added a basicjava branch for CC stringifandwhile.

---
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -0,0 +1,19 @@
+package basicjava;
+
+public class CCStringsIfAndWhile {
+	
+	public static boolean isDigit(char aChar) {
+		return false;
+		
+	}
+	
+	public static int count(String str, String chars) {
+		return 0;
+		
+	}
+	
+	public static int smallestDigit(int num) {
+		return 0;
+	}
+
+}
