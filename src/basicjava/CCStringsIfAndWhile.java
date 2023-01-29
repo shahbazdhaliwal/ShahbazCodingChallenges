f7927f25fb764367f32235ecc0209311beecfbbd
shahbazdhaliwal <107386192+shahbazdhaliwal@users.noreply.github.com>
2023-01-29 16:57:36 -0700

    Fixed some errors in previous commit. This is the one to be submitted.

--- src/basicjava/CCStringsIfAndWhile.java
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -4,9 +4,13 @@ public class CCStringsIfAndWhile {
 	
 	public static boolean isDigit(char aChar) {
 		//boolean state changes to true when char is an integer and returns the final boolean value
+		//found how to make char list from: https://www.w3schools.com/java/java_arrays.asp
 		boolean state = false;
-		if (Character.isDigit(aChar)) {
-			state = true;
+		char[] list = {'0','1','2','3','4','5','6','7','8','9'};
+		for (char ch : list) {
+			if (ch == aChar) {
+				state = true;
+			}
 		}
 		return state;
 		
@@ -16,6 +20,7 @@ public class CCStringsIfAndWhile {
 		//initializing iterative counter that keeps track of how many times a letter has occurred
 		int counter = 0;
 		//converting strings to match case, as upper case and lower case char have different ranks
+		//found how to convert strings case from: https://www.w3schools.com/java/ref_string_touppercase.asp
 		String newStr1 = str.toLowerCase();
 		String newStr2 = chars.toLowerCase();
 		//loops through every char in chars 
@@ -34,22 +39,19 @@ public class CCStringsIfAndWhile {
 	
 	public static int smallestDigit(int num) {
 		//getting rid of negative sign as it doesn't matter
-		num = Math.abs(num);
-		//finding length of number to loops over later. Found this function using:
-		// https://www.baeldung.com/java-number-of-digits-in-int
-		int length = String.valueOf(num).length();
-		
+		if (num < 0) {
+			num *= -1;
+		}		
 		// initializing variables
-		int i = 0;
 		int num1 = 0;
 		int smallestdig = 0;
-		// largestnum serves as a reference, reference is changed every time a smaller value is found
+		// following variable serves as a reference, reference is changed every time a smaller value is found
 		int largestnum = 9;
 		int returnvalue = 0;
 		
 		//loop cuts off the last digit and analyzes if its the smallest value yet encountered
-		//once smallest value is found it stores it as returnvalue, and returns returnvalue in the end
-		while (i < length) {
+		//once smallest value is found it stores it as a variable, and returns it in the end
+		while (num > 0) {
 			num1 = num % 10;
 			smallestdig = num1;
 			if (smallestdig < largestnum) {
@@ -57,7 +59,6 @@ public class CCStringsIfAndWhile {
 				returnvalue = smallestdig;
 			}
 			num /= 10;
-			i +=1;
 		}
 		return returnvalue;
 	}
