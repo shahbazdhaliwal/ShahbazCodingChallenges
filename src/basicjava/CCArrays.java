3abd664cf29474a56e7a4a1a6621dba423adf551
shahbazdhaliwal <107386192+shahbazdhaliwal@users.noreply.github.com>
2023-02-22 17:37:31 -0700

    Finished Arrays

--- src/basicjava/CCArrays.java
+++ src/basicjava/CCArrays.java
@@ -1,11 +1,34 @@
 package basicjava;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class CCArrays {
+	
+	/**
+	 * In a given charArray[], method replaces any char to a char of choice
+	 * @param charArray charArray[]
+	 * @param toReplace char in charArray[] you want replaced
+	 * @param replaceWith char you want to replace it with
+	 */
 	public static void replace(char[] charArray, char toReplace, char replaceWith) {
 		
+		String strToReplace = ("" + toReplace).toLowerCase();
+		int counter = 0;
+		
+		for (char i : charArray) {
+			String newI = ("" + i).toLowerCase();
+			if (newI.equals(strToReplace)) {
+				charArray[counter] = replaceWith;
+			}
+			counter ++;
+		}
 	}
 	
+	
 	public static void sortAlphabetic(String[] strArray) {
+		//learned from http://www.java2s.com/Tutorials/Java/java.lang/String/Java_String_CASE_INSENSITIVE_ORDER.htm
+		Arrays.sort(strArray, String.CASE_INSENSITIVE_ORDER);		
 		
 	}
 
