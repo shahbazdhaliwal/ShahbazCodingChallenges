a476202a58d7970a2a12c96590a21a6c2bc2ce02
shahbazdhaliwal <107386192+shahbazdhaliwal@users.noreply.github.com>
2023-02-25 20:05:14 -0700

    Initializing CC Array List

---
+++ src/basicjava/CCArrayList.java
@@ -0,0 +1,15 @@
+package basicjava;
+
+import java.util.ArrayList;
+
+public class CCArrayList {
+	
+	public static int indexOfIgnoreCase(ArrayList<String> strs, String toFind) {
+		return 0;
+	}
+	
+	public static void insert(ArrayList<Double> nums, double numToInsert, int insertAtIndex) {
+		
+	}
+
+}
