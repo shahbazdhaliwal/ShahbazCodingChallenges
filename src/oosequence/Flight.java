2c33dcedfc5e5565373ae059e2885edb8d7802fa
shahbazdhaliwal <107386192+shahbazdhaliwal@users.noreply.github.com>
2023-03-04 01:26:05 -0700

    Finished encapsulation

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -2,31 +2,95 @@ package oosequence;
 
 import java.util.Date;
 
+/**
+ * Represents a flight
+ * @author Shahbaz
+ *
+ */
 public class Flight {
 	
-	Date departure;
-	Date arrival;
+	//initializing instance variables
+	private Date departure;
+	private Date arrival;
 	
+	/**
+	 * constructor that defines instance variables
+	 * @param departureDate departure date
+	 * @param arrivalDate arrival date
+	 */
 	Flight (Date departureDate, Date arrivalDate) {
 		if ((departureDate != null && arrivalDate != null) && 
 				departureDate.before(arrivalDate)) {
-			departure = departureDate;
-			arrival = arrivalDate;
+			setDeparture(departureDate);
+			setArrival(arrivalDate);
+		} else if (departureDate != null && arrivalDate == null) {
+			setDeparture(departureDate);
+		} else if (arrivalDate != null && departureDate == null) {
+			setArrival(arrivalDate);
 		}
 	}
 	
+	/**
+	 * constructor for making copy of some give flight
+	 * @param toCopy flight you want to copy
+	 */
 	Flight(Flight toCopy){
-		departure = toCopy.departure;
-		arrival = toCopy.arrival;
+		setDeparture(toCopy.getDeparture());
+		setArrival(toCopy.getArrival());
 	}
 	
+	/**
+	 * returns the length of the flight
+	 * @return length
+	 */
 	long length() {
-		if ((departure != null && arrival != null) && 
-				departure.before(arrival)) {
-			long time = (arrival.getTime() - departure.getTime()) / 60000;  
+		if ((getDeparture() != null && getArrival() != null) && 
+				getDeparture().before(getArrival())) {
+			//find the difference between arrival and departure
+			long time = (getArrival().getTime() - getDeparture().getTime()) / 60000;  
 			return time;
 		} else {
 			return (long) 0.0;
+		}
+	}
+	
+	/**
+	 * returns departure time
+	 * @return departure time
+	 */
+	Date getDeparture() {
+		return departure;
+	}
+	
+	/**
+	 * sets the departure of the flight granted the time of departure is valid
+	 * @param departure departure
+	 */
+	void setDeparture(Date departure) {
+		if (this.arrival != null && departure.before(this.arrival)) {
+			this.departure = departure;
+		} else if (this.arrival == null) {
+			this.departure = departure;
+		}
+	}
+	
+	/**
+	 * returns the arrival time
+	 * @return arrival time
+	 */
+	Date getArrival() {
+		return arrival;
+	}
+
+	/**
+	 * sets the arrival time of a flight given its a valid time
+	 * @param arrival arrival time
+	 */
+	void setArrival(Date arrival) {
+		if (this.departure != null && arrival.after(this.departure)) {
+			this.arrival = arrival;
+		} else if (this.departure == null) {
+			this.arrival = arrival;
 		}
 	}
 	
---
+++ src/oosequence/Itinerary.java
@@ -0,0 +1,100 @@
+package oosequence;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Stores a persons flight itinerary with departure and arrival times
+ * @author Shahbaz
+ *
+ */
+public class Itinerary {
+	
+	
+	
+	//Initializing instance variables
+	private ArrayList<Flight> flights;
+	private String name;
+	
+	/**
+	 * constructor that defines instance variables
+	 * @param nameOfItinerary name of the itinerary
+	 */
+	Itinerary (String nameOfItinerary) {
+		name = nameOfItinerary;
+		flights = new ArrayList<Flight>();
+		
+	}
+		
+	/**
+	 * adds a flight to the itinerary after checking for flight overlap and sorting the itinerary list with first departure first
+	 * @param flightToAdd flight you want to add to the itinerary
+	 */
+	void addFlight(Flight flightToAdd) {
+		Date newDeparture = flightToAdd.getDeparture();
+		Date newArrival = flightToAdd.getArrival();
+		boolean overlapFlag = false;
+		
+		//checks for overlap
+		for (Flight existingFlight : flights) {
+			//if statement for all cases where there could be overlap
+			if (existingFlight.getDeparture().after(newDeparture) && existingFlight.getDeparture().before(newArrival) ||
+					existingFlight.getArrival().after(newDeparture) && existingFlight.getArrival().before(newArrival) ||
+					existingFlight.getDeparture().equals(newDeparture) || existingFlight.getArrival().equals(newArrival)) {
+				overlapFlag = true;
+			}
+		}
+		
+		//sorting list and adding new flight
+		if (overlapFlag == false) {
+			//case for if list is empty of if new departure after last current arrival time
+			if ( flights.size() == 0 || newDeparture.after(flights.get(flights.size() - 1).getArrival()) ) {
+				flights.add(flightToAdd);
+			} else {
+				for (int i = 0; i < flights.size(); i ++) {
+					//checks if new arrival is before current flights departure and puts new flight in front of current flight if 
+					// 	    thats the case
+					if (newArrival.before(flights.get(i).getDeparture())) {
+						flights.add(i, flightToAdd);
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * finds the total time a person will spend in between flights
+	 * @return layover
+	 */
+	long getTotalLayover() {
+		long layover = 0L;
+		if (flights.size() > 1) {
+			for (int counter = 0; counter < flights.size() - 1; counter ++) {
+				//adds the difference between arrival and next departure for every element in list 
+				Flight nextFlight = flights.get(counter + 1);
+				Flight currentFlight = flights.get(counter);
+				layover += ((nextFlight.getDeparture().getTime() 
+						- currentFlight.getArrival().getTime()) / 60000);
+			}	
+		}
+		return layover;
+	}
+
+	/**
+	 * gives the list of flights 
+	 * @return flights
+	 */
+	ArrayList<Flight> getFlights() {
+		return flights;
+	}
+	
+	/**
+	 * gives the name of the itinerary
+	 * @return name
+	 */
+	String getName() {
+		return name;
+	}
+
+}
