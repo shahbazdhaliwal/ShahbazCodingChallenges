97dcd789acdfff1e856ce01f2c81917fa8fa12ca
shahbazdhaliwal <107386192+shahbazdhaliwal@users.noreply.github.com>
2023-03-11 14:40:21 -0700

    fixed bug

---
+++ src/basicjava/module-info.java
@@ -0,0 +1,9 @@
+module GradeCalculator {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	requires junit;
+	
+	//opens application to javafx.graphics, javafx.fxml;
+}
